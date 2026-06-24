6ac051aad2d9b9c3edc1329cb813d0426f6c8d61
njharlen <njharlen@broadinstitute.org>
2026-06-24 16:01:43 +0000

    rerun

--- figures/figure_params.py
+++ figures/figure_params.py
@@ -1,3 +1,9 @@
 grade_colors = {'low' : 'blue','high' : 'orangered'}
 
-disease_colors = {'FL' : '#2020df','tFL' : '#df2020','FL3B':'orange','FLBCL':'#520269','DLBCL':'#1e5e02'}
+disease_colors = {'FL' : '#2020df',
+                  'tFL' : '#df2020',
+                  'FL3B':'orange',
+                  'FLBCL':'#520269',
+                  'DLBCL':'#1e5e02',
+                 'DLBCL-nonGCB' : 'magenta',
+                 'DLBCL-GCB' : 'purple'}
